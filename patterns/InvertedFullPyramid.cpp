cc37eee18d7874fd5921f8cb79841f171d25bae3
Ayush Sahay <sahayayush385@gmail.com>
2024-02-21 00:06:13 +0530

    code commit

---
+++ patterns/InvertedFullPyramid.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the no. of rows: ";
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        for(int j=0; j < i; j++){
+            cout << " ";
+        }
+        for(int j=n; j>=i; j--){
+            cout << "& ";
+        }
+
+        cout << endl;
+    }
+}
