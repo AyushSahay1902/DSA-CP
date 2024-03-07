a3c20019eb97bbae0815578eeb516848a53ddffe
Ayush Sahay <sahayayush385@gmail.com>
2024-03-07 23:49:31 +0530

    code commit

--- Basics/MajorityElement.cpp
+++ Basics/MajorityElement.cpp
@@ -1,27 +1,43 @@
 #include<iostream>
 using namespace std;
 #include<vector>
+#include<map>
 
 //using a hashmap in cpp it will take O(n logn)
 //More Voting Algorithm - cnt = 0, ele = 0,
 // 7,7,2,3,6,7,7,2,3,5,7,7,5
 // 1, 2, 1, 0, 1, 0, 1, 0, 1, 0, 1, 2, 1 - Majority is 7
-class Solution{
-    public:
-    int MajorityElement(vector<int> &nums){
-        int count = 0;
-        int currEle = 0;
+//Hashing Solution
+class Solution {
+public:
+    int MajorityElement(vector<int>& nums) {
+        map<int, int> freqMap;
 
-        for(int num: nums){
-            if(count == 0){
-                currEle = num;
+        for (int num : nums) {
+            if (++freqMap[num] > nums.size() / 2) {
+                return num;
             }
-            if(num == currEle) count += 1;
-            else count -= 1;
         }
-        return currEle;
+
+        return -1;
     }
 };
+// class Solution{
+//     public:
+//     int MajorityElement(vector<int> &nums){
+//         int count = 0;
+//         int currEle = 0;
+
+//         for(int num: nums){
+//             if(count == 0){
+//                 currEle = num;
+//             }
+//             if(num == currEle) count += 1;
+//             else count -= 1;
+//         }
+//         return currEle;
+//     }
+// };
 int main(){
     vector<int> nums = {7,7,2,3,6,7,7,2,3,5,7,7,5};
     Solution sol;
